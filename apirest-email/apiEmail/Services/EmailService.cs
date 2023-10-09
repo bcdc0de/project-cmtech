1f56802993830c5fa97c92ba997d8cf801cb2c67
bcdc0de <bcd@cin.ufpe.br>
2023-10-08 22:58:09 -0300

    modificação

---
+++ apirest-email/apiEmail/Services/EmailService.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using Npgsql;
+using MimeKit;
+
+namespace RpaEmail.Services
+{
+    public class EmailService
+    {
+        private readonly string _connectionString;
+
+        public EmailService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void InsertMessages(List<MimeMessage> messages)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                foreach (var message in messages)
+                {
+                    using (var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection = connection;
+                        cmd.CommandText = "INSERT INTO mensagens (assunto, remetente, destinatario, data, corpo_texto) VALUES (@assunto, @remetente, @destinatario, @data, @corpo_texto)";
+                        cmd.Parameters.AddWithValue("assunto", message.Subject);
+                        cmd.Parameters.AddWithValue("remetente", message.From.ToString());
+                        cmd.Parameters.AddWithValue("destinatario", message.To.ToString());
+                        cmd.Parameters.AddWithValue("data", message.Date);
+                        cmd.Parameters.AddWithValue("corpo_texto", message.TextBody);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+    }
+}
